e1da1244f79095980ac6d7335e20ce07422e41bc
michi <michi@dermader.com>
2026-01-18 13:03:21 +0100

    feat: Add entity metadata with data_path for automatic HA mapping

--- LilyGo-HiGrow/src/modules/homeassistant.cpp
+++ LilyGo-HiGrow/src/modules/homeassistant.cpp
@@ -1,5 +1,8 @@
 // Filename: homeassistant.cpp
-// Home Assistant Integration Implementation
+// V1.4 ESP32 sendet data_path in Entity-Metadaten für automatisches Mapping
+// V1.3 ESP32 sendet Entity-Metadaten (type, device_class, unit) an HA
+// V1.2 Home Assistant Integration Implementation
+// V1.1 Initial
 
 #include "homeassistant.h"
 
@@ -88,11 +91,13 @@ void HomeAssistantIntegration::setupRestAPI() {
 }
 
 //*********************************
-// Endpoint 1: Device Info
+// Endpoint 1: Device Info + Entity Metadata
+// ESP32 sagt HA was was ist UND wo die Daten liegen!
 //*********************************
 void HomeAssistantIntegration::handleDeviceInfo(AsyncWebServerRequest *request) {
-    StaticJsonDocument<512> doc;
+    StaticJsonDocument<2560> doc;
     
+    // Device Information
     doc["name"] = DEVICE_NAME;
     doc["model"] = DEVICE_MODEL;
     doc["version"] = FIRMWARE_VERSION;
@@ -100,6 +105,111 @@ void HomeAssistantIntegration::handleDeviceInfo(AsyncWebServerRequest *request) 
     doc["id"] = device_id;
     doc["type"] = "irrigation_controller";
     doc["hostname"] = String(MDNS_HOSTNAME) + ".local";
+    
+    // Entity Definitions mit data_path für automatisches Mapping
+    JsonArray entities = doc.createNestedArray("entities");
+    
+    // Sensor: Bodenfeuchte
+    JsonObject soil_moisture = entities.createNestedObject();
+    soil_moisture["id"] = "bodenfeuchte";
+    soil_moisture["name"] = "Bodenfeuchte";
+    soil_moisture["type"] = "sensor";
+    soil_moisture["device_class"] = "moisture";
+    soil_moisture["unit"] = "%";
+    soil_moisture["state_class"] = "measurement";
+    JsonArray sm_path = soil_moisture.createNestedArray("data_path");
+    sm_path.add("soil");
+    sm_path.add("moisture");
+    
+    // Sensor: Salzgehalt
+    JsonObject salt = entities.createNestedObject();
+    salt["id"] = "salzgehalt";
+    salt["name"] = "Salzgehalt";
+    salt["type"] = "sensor";
+    salt["device_class"] = "voltage";
+    salt["unit"] = "mV";
+    salt["state_class"] = "measurement";
+    JsonArray salt_path = salt.createNestedArray("data_path");
+    salt_path.add("soil");
+    salt_path.add("salt");
+    
+    // Sensor: Batterie
+    JsonObject battery = entities.createNestedObject();
+    battery["id"] = "batterie";
+    battery["name"] = "Batterie";
+    battery["type"] = "sensor";
+    battery["device_class"] = "battery";
+    battery["unit"] = "%";
+    battery["state_class"] = "measurement";
+    JsonArray bat_path = battery.createNestedArray("data_path");
+    bat_path.add("battery");
+    bat_path.add("percent");
+    
+    // Sensor: Temperatur (wenn vorhanden)
+    if (sensorMgr->has_dht11 || sensorMgr->has_sht3xSensor || sensorMgr->has_ds18b20) {
+        JsonObject temp = entities.createNestedObject();
+        temp["id"] = "temperatur";
+        temp["name"] = "Temperatur";
+        temp["type"] = "sensor";
+        temp["device_class"] = "temperature";
+        temp["unit"] = "°C";
+        temp["state_class"] = "measurement";
+        JsonArray temp_path = temp.createNestedArray("data_path");
+        temp_path.add("temperature");
+        temp_path.add("value");
+    }
+    
+    // Sensor: Luftfeuchtigkeit (wenn vorhanden)
+    if (sensorMgr->has_dht11 || sensorMgr->has_sht3xSensor) {
+        JsonObject humidity = entities.createNestedObject();
+        humidity["id"] = "luftfeuchtigkeit";
+        humidity["name"] = "Luftfeuchtigkeit";
+        humidity["type"] = "sensor";
+        humidity["device_class"] = "humidity";
+        humidity["unit"] = "%";
+        humidity["state_class"] = "measurement";
+        JsonArray hum_path = humidity.createNestedArray("data_path");
+        hum_path.add("humidity");
+        hum_path.add("value");
+    }
+    
+    // Sensor: Helligkeit (wenn vorhanden)
+    if (sensorMgr->has_lightSensor) {
+        JsonObject light = entities.createNestedObject();
+        light["id"] = "helligkeit";
+        light["name"] = "Helligkeit";
+        light["type"] = "sensor";
+        light["device_class"] = "illuminance";
+        light["unit"] = "lx";
+        light["state_class"] = "measurement";
+        JsonArray light_path = light.createNestedArray("data_path");
+        light_path.add("light");
+        light_path.add("lux");
+    }
+    
+    // Switch: Pumpe
+    JsonObject pump_switch = entities.createNestedObject();
+    pump_switch["id"] = "pumpe";
+    pump_switch["name"] = "Pumpe";
+    pump_switch["type"] = "switch";
+    pump_switch["device_class"] = "switch";
+    JsonArray pump_path = pump_switch.createNestedArray("data_path");
+    pump_path.add("pump");
+    pump_path.add("running");
+    
+    // Number: Pumpenleistung
+    JsonObject pump_pwm = entities.createNestedObject();
+    pump_pwm["id"] = "pumpenleistung";
+    pump_pwm["name"] = "Pumpenleistung";
+    pump_pwm["type"] = "number";
+    pump_pwm["device_class"] = "power_factor";
+    pump_pwm["unit"] = "%";
+    pump_pwm["min"] = 0;
+    pump_pwm["max"] = 100;
+    pump_pwm["step"] = 1;
+    JsonArray pwm_path = pump_pwm.createNestedArray("data_path");
+    pwm_path.add("pump");
+    pwm_path.add("pwm_target");
     
     String response;
     serializeJson(doc, response);
@@ -134,7 +244,7 @@ void HomeAssistantIntegration::createStatusJSON(String& output) {
     // Battery
     sensorMgr->getSensorEvent(VOLTAGE_SENSOR_ID, val);
     doc["battery"]["voltage"] = val.voltage;
-    doc["battery"]["percent"] = map(val.voltage, 3300, 4200, 0, 100);
+    int battPercent = map(val.voltage, 3300, 4200, 0, 100); if (battPercent > 100) battPercent = 100; if (battPercent < 0) battPercent = 0; doc["battery"]["percent"] = battPercent;
     
     // Temperature & Humidity
     if (sensorMgr->has_dht11) {
